ad804173fae466185e66c85df780deea33811d36
Md. Shafin Mahmud Chowdhury <87469950+shafinchowdhury@users.noreply.github.com>
2024-05-19 11:12:47 +0600

    Sum of Digits of a Five Digit Number.c

--- hackerrank/c/Sum of Digits of a Five Digit Number.c
+++ hackerrank/c/Sum of Digits of a Five Digit Number.c
@@ -1,1 +1,18 @@
 
+#include <stdio.h>
+
+int main ()
+{
+    int num, sum = 0, div;
+    scanf("%d",&num);
+
+    for(int i = 1; i <= 5; i++){
+        div = num % 10; // div = remainder
+        sum += div;
+        num = num/10;
+    }
+
+    printf("%d",sum);
+return 0;
+}
+
