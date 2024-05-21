d4e535b9ede7952bcdd9855be9a7ab2a51aa89ea
Md. Shafin Mahmud Chowdhury <shafinchowdhury2003@gmail.com>
2024-05-21 23:52:00 +0600

    modifed

--- hackerrank/c/Digit Frequency.c
+++ hackerrank/c/Digit Frequency.c
@@ -1,38 +1,118 @@
-
-// https://www.hackerrank.com/challenges/printing-pattern-2/problem?isFullScreen=true
-
 #include <stdio.h>
+#include <string.h>
 
-int main( )
+int main ()
 {
-    int num;
-    scanf("%d",&num);
-    int range = (num*2)-1;
-    int arr[range][range];
+    char arr[100000];
+    scanf("%s",arr);
+    int length = strlen(arr);
 
+    int sum_0=0,sum_1=0,sum_2=0,sum_3=0,sum_4=0,sum_5=0,sum_6=0,sum_7=0,sum_8=0,sum_9=0;
 
-    for(int i = 0; i < range; i++){
-        for(int j = 0; j < range; j++){
-            int r = num - i;
-            int c = num - j;
-            if(r>=1){
-                r=r;
-            }
-            else{
-                r = (i - num) + 2;
-            }
-            if(c>=1){
-                c=c;
-            }
-            else{
-                c = (j - num) + 2;
-            }
-                int max = (r>c) ? (r):(c);
-                printf("%d ",max);
-            }
-            printf("\n");
+    // Zero
+    for(int i = 0; i <= length; i++){
+        if(arr[i] == '0'){
+            sum_0 += 1;
         }
+        else{
+            continue;
+        }
+    }
 
+
+    // One
+        for(int i = 0; i <= length; i++){
+        if(arr[i] == '1'){
+            sum_1 += 1;
+        }
+        else{
+            continue;
+        }
+    }
+
+    // Two
+    for(int i = 0; i <= length; i++){
+        if(arr[i] == '2'){
+            sum_2 += 1;
+        }
+        else{
+            continue;
+        }
+    }
+
+    // Three
+    for(int i = 0; i <= length; i++){
+        if(arr[i] == '3'){
+            sum_3 += 1;
+        }
+        else{
+            continue;
+        }
+    }
+
+    //Four
+    for(int i = 0; i <= length; i++){
+        if(arr[i] == '4'){
+            sum_4 += 1;
+        }
+        else{
+            continue;
+        }
+    }
+
+    // Five
+    for(int i = 0; i <= length; i++){
+        if(arr[i] == '5'){
+            sum_5 += 1;
+        }
+        else{
+            continue;
+        }
+    }
+
+    // six
+        for(int i = 0; i <= length; i++){
+        if(arr[i] == '6'){
+            sum_6 += 1;
+        }
+        else{
+            continue;
+        }
+    }
+
+    //seven
+        for(int i = 0; i <= length; i++){
+        if(arr[i] == '7'){
+            sum_7 += 1;
+        }
+        else{
+            continue;
+        }
+    }
+
+    //eight
+        for(int i = 0; i <= length; i++){
+        if(arr[i] == '8'){
+           sum_8 += 1;
+        }
+        else{
+            continue;
+        }
+    }
+
+    //nine
+        for(int i = 0; i <= length; i++){
+        if(arr[i] == '9'){
+            sum_9 += 1;
+        }
+        else{
+            continue;
+        }
+    }
+
+
+    printf("%d %d %d %d %d %d %d %d %d %d ",sum_0,sum_1,sum_2,sum_3,sum_4,sum_5,sum_6,sum_7,sum_8,sum_9);
 
 return 0;
 }
+
