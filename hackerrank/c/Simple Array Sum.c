f340dc307859343940aaafc1df86308975d30ae1
Md. Shafin Mahmud Chowdhury <shafinchowdhury2003@gmail.com>
2024-06-25 05:43:51 +0600

    Update Simple Array Sum.c

--- hackerrank/c/Simple Array Sum.c
+++ hackerrank/c/Simple Array Sum.c
@@ -10,6 +10,19 @@ int main()
     scanf("%d",&b[i]);
   }
 
+  int score_a = 0;
+  int score_b = 0;
+  for(int i = 0; i < 3; i++){
+    if(a[i] > b[i]){
+        score_a++;
+    }
+    else if(a[i] < b[i]){
+        score_b++;
+    }
+
+  }
+
+  printf("%d %d",score_a,score_b);
 return 0;
 }
 
