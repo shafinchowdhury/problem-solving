5d8760592c46439ce13af70604d0c8e5e7bf235e
Md. Shafin Mahmud Chowdhury <shafinchowdhury2003@gmail.com>
2024-06-25 05:38:15 +0600

    Create Simple Array Sum.c

---
+++ hackerrank/c/Simple Array Sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  int a[3] , b[3];
+  for(int i = 0; i < 3; i++){
+    scanf("%d",&a[i]);
+  }
+  for(int i = 0; i < 3; i++){
+    scanf("%d",&b[i]);
+  }
+
+return 0;
+}
+
