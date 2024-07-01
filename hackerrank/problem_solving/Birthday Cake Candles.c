9f47af8367266d7609f22fe362e804238075bf9d
Md. Shafin Mahmud Chowdhury <shafinchowdhury2003@gmail.com>
2024-07-01 16:21:30 +0600

    Create Birthday Cake Candles.c

---
+++ hackerrank/problem_solving/Birthday Cake Candles.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        scanf("%d",&arr[i]);
+    }
+    int max = arr[0];
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] >= max){
+            max = arr[i];
+        }
+    }
+    int count;
+    for(int i = 0; i < n; i++){
+        if(max == arr[i]){
+            count++;
+        }
+    }
+    printf("%d",count);
+
+
+
+
+return 0;
+}
